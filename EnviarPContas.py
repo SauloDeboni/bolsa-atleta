4b0b50fafbd307dbe0a78d315567095b3f933490
saulodeboni <saulodeboni@gmail.com>
2022-06-08 10:17:08 -0300

    EnviarPContas atualizado

--- EnviarPContas.py
+++ EnviarPContas.py
@@ -10,21 +10,29 @@ import pandas as pd
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 
-def EnviarPContas(path):
+def EnviarPContas(ano, data):
     
+    ano_solicitacao = str(ano)
+    data_arquivo = str(data)
+    
+    # Busca o arquivo no servidor
+    arquivo_lista = "Prestacao-" + data_arquivo + ".xlsx"
+    arquivo_lista_path = "Z:/Documentos/Ministério do Esporte/Bolsa Atleta/Prestação de Contas/Dados Tabulados/Prestação " + ano_solicitacao + "/" + arquivo_lista
+    
+        
     # Importa o arquivo com os contatos e transforma em dataframe
-    lista_envio = pd.read_excel(path)
+    lista_envio = pd.read_excel(arquivo_lista_path)
     
     for index in lista_envio.index:
         
         # Busca os dados do contato no dataframe
         nome_completo = str(lista_envio["Nome Completo"][index])
-        ano = str(lista_envio["Qual o ano de recebimento do Bolsa Atleta?"][index])
+        ano_recebimento = str(lista_envio["Qual o ano de recebimento do Bolsa Atleta?"][index])
         email = str(lista_envio["E-Mail"][index])
         
         # Gera o path e nome do arquivo para ser anexado
-        arquivo = ano + " " + nome_completo + ".pdf"
-        arquivo_path = "Z:/Documentos/Ministério do Esporte/Bolsa Atleta/Prestação de Contas/Prestação de Contas " + ano + "/" + arquivo
+        arquivo_anexo = ano_recebimento + " " + nome_completo + ".pdf"
+        arquivo_anexo_path = "Z:/Documentos/Ministério do Esporte/Bolsa Atleta/Prestação de Contas/Prestação de Contas " + ano_recebimento + "/" + arquivo_anexo
 
         message = Mail(
             from_email="inscricoes@remobrasil.com",
@@ -40,7 +48,7 @@ def EnviarPContas(path):
         message.template_id = "d-05b5c69596134834b49cf68702d97396"
         
         # Anexando o arquivo
-        with open(arquivo_path, "rb") as f:
+        with open(arquivo_anexo_path, "rb") as f:
             data = f.read()
             f.close()
 
@@ -48,7 +56,7 @@ def EnviarPContas(path):
 
         attachedFile = Attachment(
             FileContent(encoded_file),
-            FileName(arquivo),
+            FileName(arquivo_anexo),
             FileType("application/pdf"),
             Disposition("attachment")
         )
@@ -64,4 +72,4 @@ def EnviarPContas(path):
             print(e.message)
 
 if __name__ == "__main__":
-    EnviarPContas(path=input("Qual o caminho do arquivo com as solicitações? "))
+    EnviarPContas(ano=input("Qual o ano das solicitações? "),data=input("Qual a data do arquivo com as solicitações? (formato aaaa/mm/dd) "))
